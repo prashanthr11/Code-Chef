78e42781dc2e7d18f9ff4c68ea09381d5ca47c55
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2019-12-17 19:30:20 +0530

    Add files via upload

---
+++ WATSCORE.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--) {
+        int maximum = 0;
+        int n;
+        // n = rand();
+        cin >> n;
+        int a[n],b[n];
+        map<int,int> mp;
+        // map<int,int> smp;
+        for(int i=0;i<n;i++){
+            cin >> a[i] >> b[i];
+        }
+        for(int i=0;i<n;i++){
+            if(mp[a[i]]){
+                mp[a[i]] = mp[a[i]] > b[i] ? mp[a[i]] : b[i];
+            }
+            else if(b[i] == 0){
+                mp[a[i]] =  mp[a[i]] > b[i] ? mp[a[i]] : b[i];
+            }
+            else {
+            mp[a[i]] = b[i];
+            }
+        }
+        // for(auto &i:mp) cout << i.first << ' ' << i.second << endl;
+        // cout << endl << endl << endl;
+        // for(auto &i:smp)
+        // {
+        //     // if(smp[a[i]]){
+        //         cout << i.first << ' ' << i.second << endl;
+        //     // }
+        // }
+        for(auto i:mp){
+            if(i.first < 9)
+            maximum+= i.second;
+        }
+        cout << maximum << endl;
+        // for(int i=0;i<n;i++){
+        //     cout << a[i] << ' ' << b[i] <<endl;
+    // }
+    }
+}
