2ee776f8f5cba6f5817e230a7084c660dedf2f4c
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2019-12-25 12:17:45 +0530

    Create CEQUAL.cpp

---
+++ CEQUAL.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+# include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        
+        bool flag = true;
+        map < int, int > mp;
+        for(int i =0 ;i <n; i++) {
+            int a;
+            cin >> a;
+            mp[a]++;
+        }
+        for(auto i:mp) {
+            if(i.second > 1) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+        cout << "No" << endl;
+        else cout <<  "Yes" << endl;
+    }
+}
