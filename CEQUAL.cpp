e9c9af652009a500cbf48c8660f09306baa1c222
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-04-19 01:26:23 +0530

    Update CEQUAL.cpp

--- CEQUAL.cpp
+++ CEQUAL.cpp
@@ -1,5 +1,5 @@
 # include <iostream>
-# include<bits/stdc++.h>
+# include <bits/stdc++.h>
 using namespace std;
 int main() {
     int t;
@@ -9,8 +9,8 @@ int main() {
         cin >> n;
         
         bool flag = true;
-        map < int, int > mp;
-        for(int i =0 ;i <n; i++) {
+        map<int, int> mp;
+        for(int i = 0; i < n; ++i) {
             int a;
             cin >> a;
             mp[a]++;
